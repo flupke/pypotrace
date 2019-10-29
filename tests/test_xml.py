889bf1fa1cef18ee7f5a40249c053f6700b4f16f
Mehmet Güngören <mehgungoren@gmail.com>
2019-10-29 11:52:25 +0300

    fix test error of to_xml

--- tests/test_xml.py
+++ tests/test_xml.py
@@ -3,21 +3,21 @@ import numpy as np
 import potrace
 
 
-circle_xml = '<?xml version="1.0" standalone="no"?>'
-'<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN"'
-' "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">'
-'<svg version="1.0" xmlns="http://www.w3.org/2000/svg"'
-' width="32.000000pt" height="32.000000pt" viewBox="0 0 32.000000 32.000000"'
-' preserveAspectRatio="xMidYMid meet">'
-'<metadata>'
-'Created by potrace potracelib 1.16, written by Peter Selinger 2001-2015'
-'</metadata>'
-'<g transform="scale(0.100000,0.100000)"'
-'fill="#000000" stroke="none">'
-'<path d="M133 235 c-62 -26 -59 -120 4 -143 67 -23 125 40 98 105 -17 40 -60'
-'56 -102 38z"/>'
-'</g>'
-'</svg>'
+circle_xml = '<?xml version="1.0" standalone="no"?>\n' \
+            '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN"\n' \
+            ' "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">\n' \
+            '<svg version="1.0" xmlns="http://www.w3.org/2000/svg"\n' \
+            ' width="32.000000pt" height="32.000000pt" viewBox="0 0 32.000000 32.000000"\n' \
+            ' preserveAspectRatio="xMidYMid meet">\n' \
+            '<metadata>\n' \
+            'Created by potrace {}, written by Peter Selinger 2001-2015\n' \
+            '</metadata>\n' \
+            '<g transform="scale(0.100000,0.100000)"\n' \
+            'fill="#000000" stroke="none">\n' \
+            '<path d="M133 235 c-62 -26 -59 -120 4 -143 67 -23 125 40 98 105 -17 40 -60\n' \
+            '56 -102 38z"/>\n' \
+            '</g>\n' \
+            '</svg>'
 
 
 def test_to_xml():
@@ -36,4 +36,6 @@ def test_to_xml():
     # Trace it
     bmp = potrace.Bitmap(data)
     bmp.trace()
-    assert_equal(circle_xml, bmp.to_xml())
+    version = potrace.potracelib_version()
+    xml = circle_xml.format(version)
+    assert_equal(xml, bmp.to_xml())
